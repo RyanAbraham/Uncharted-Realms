54252244864c137960e88d6468bc576a2baad9bb
RyanAbraham <ryan_ja@live.ca>
2018-04-23 21:49:43 -0400

    Full Card UI implementation

--- src/scenes/Game/Card/components/Card.js
+++ src/scenes/Game/Card/components/Card.js
@@ -6,9 +6,14 @@ class Card extends Component {
         return (
             <div className="card">
                 <div className="card__name">{this.props.cardDetails.name}</div>
-                <div className="card__pow">{this.props.cardDetails.pow}</div>
+                <div className="card__clock">{this.props.cardDetails.clk}</div>
+                <img className="card__img" src={this.props.cardDetails.img} alt="Card Artwork" />
+                <div className="card__stats">
+                <div className={"card__power " + (this.props.pow >= 5 ? "card__power--long" : "")}>
+                    {this.props.cardDetails.pow}
+                </div>
                 <div className="card__hp">{this.props.cardDetails.hp}</div>
-                <img src={this.props.cardDetails.img} alt="Card Artwork" />
+                </div>
             </div>
         );
     }
