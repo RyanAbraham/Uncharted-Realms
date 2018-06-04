3f926ef524c1b833dbc7b5272965e4b57ef79dfc
RyanAbraham <ryan_ja@live.ca>
2018-06-03 22:30:16 -0400

    Add landing

--- src/scenes/Landing/Landing.js
+++ src/scenes/Landing/Landing.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Typography } from "material-ui";
+import "./Landing.css";
 
 const LandingPage = () => (
-  <div>
+  <div className="ur-landing">
     <Typography>
       Welcome to Uncharted Realms!
     </Typography>
