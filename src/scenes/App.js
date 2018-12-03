e67025b70f4931d8387e13435601feb1a4eb78a9
RyanAbraham <ryan_ja@live.ca>
2018-12-03 17:50:00 -0500

    Import cleanup

--- src/scenes/App.js
+++ src/scenes/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import { Router, Route } from "react-router-dom";
+import { firebase } from "../services/firebase";
+import rq from "request-promise";
+
+import { LinearProgress } from "material-ui";
+import blue from "material-ui/colors/blue";
+import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
+
 import LandingPage from "./Landing/scenes/Landing";
 import EntryPage from "./Entry/scenes/Entry";
 import PasswordResetPage from "./Account/scenes/PasswordReset";
 import HomePage from "./Home/Home";
 import Game from "./Game/scenes/Game";
 import AccountPage from "./Account/scenes/Account";
-import * as routes from "../constants/routes";
-import { firebase } from "../services/firebase";
-import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
-import blue from "material-ui/colors/blue";
-import { LinearProgress } from "material-ui";
 import NavBar from "./NavBar/NavBar";
+import * as routes from "../constants/routes";
 import history from "./history.js";
 
 class App extends Component {
@@ -31,6 +34,20 @@ class App extends Component {
 
   componentDidMount() {
     firebase.auth.onAuthStateChanged((authUser, _error) => {
+      // if (authUser) {
+      //   const options = {
+      //     method: "POST",
+      //     uri: "http://localhost:5000/auth/login",
+      //     body: {
+      //       fbToken: authUser,
+      //     },
+      //     json: true,
+      //   };
+      //   return rq(options)
+      //     .then(body => {
+      //       console.log("### body", body);
+      //     });
+      // }
       this.setState({
         authUser,
         isLoading: false,
